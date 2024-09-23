8898924f9bf7bb9a9b083cfea935c2f2771c5bce
m104 <m104ngc4594@sina.com>
2024-09-23 13:08:33 +0800

    feature: support metadata service

---
+++ crm-metadata/src/lib.rs
@@ -0,0 +1,45 @@
+pub mod pb;
+
+mod abi;
+mod config;
+
+pub use config::AppConfig;
+
+use futures::Stream;
+use pb::{
+    meta_data_server::{MetaData, MetaDataServer},
+    Content, MaterializeRequest,
+};
+use std::pin::Pin;
+use tonic::{async_trait, Request, Response, Status, Streaming};
+
+#[allow(unused)]
+pub struct MetadataService {
+    config: AppConfig,
+}
+
+type ServiceResult<T> = Result<Response<T>, Status>;
+type ResponseStream = Pin<Box<dyn Stream<Item = Result<Content, Status>> + Send>>;
+
+#[async_trait]
+impl MetaData for MetadataService {
+    type MaterializeStream = ResponseStream;
+
+    async fn materialize(
+        &self,
+        request: Request<Streaming<MaterializeRequest>>,
+    ) -> ServiceResult<Self::MaterializeStream> {
+        let query = request.into_inner();
+        self.materialize(query).await
+    }
+}
+
+impl MetadataService {
+    pub fn new(config: AppConfig) -> Self {
+        Self { config }
+    }
+
+    pub fn into_server(self) -> MetaDataServer<Self> {
+        MetaDataServer::new(self)
+    }
+}
